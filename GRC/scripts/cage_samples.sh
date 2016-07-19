330d2cb8ff80ecbc22563e637355590b43630f43
vysarge <vysarge@gmail.com>
2016-07-19 10:32:13 -0700

    Updated automation scripts

--- GRC/scripts/cage_samples.sh
+++ GRC/scripts/cage_samples.sh
@@ -5,9 +5,11 @@ usage="$(basename "$0") [-h] [-g gain] -n name
 Program to take samples between 72 and 400 MHz in steps of 4 MHz.
 
 where: 
-    -h|--help       show this help text
-    -g|--gain (=40) set the gain of the receiver, defaults to 0
-    -n|--name       set the name of the folder and archive in which samples will be saved"
+    -h|--help              show this help text
+    -g|--gain     (=40)    set the gain of the receiver
+    -r|--rate     (=16e6)  set the sampling rate of the receiver
+    -d|--duration (=0.1)   set the duration (seconds) during which to take samples
+    -n|--name              set the name of the folder and archive in which samples will be saved"
 
 while [[ $# -gt 0 ]] 
 do
@@ -19,6 +21,14 @@ case $key in
     ;;
     -g|--gain)
     GAIN="$2"
+    shift
+    ;;
+   -r|--rate)
+    RATE="$2"
+    shift
+    ;;
+    -d|--duration)
+    DUR="$2"
     shift
     ;;
     -h|--help)
@@ -36,15 +46,17 @@ if [ "$NAME" == "NA" ]; then
 fi
 
 GAIN=${GAIN:-40}
+RATE=${RATE:-16000000}
+DUR=${DUR:-0.1}
 
 mkdir ../samples/$NAME
 
 # cage elements
 FREQMHZ=72      # starting
-FREQMHZMAX=400  # ending
+FREQMHZMAX=400  # ending     400
 FREQMHZSTEP=4   # step
 
-~/grc/applications/take_samples/build/take_samples --file ~/grc/samples/$NAME/ --gain $GAIN --start $FREQMHZ --end $FREQMHZMAX --step $FREQMHZSTEP
+~/grc/applications/take_samples/build/take_samples --file ~/grc/samples/$NAME/ --gain $GAIN --start $FREQMHZ --end $FREQMHZMAX --step $FREQMHZSTEP --rate $RATE --duration $DUR
 #make archive and clean up
 ARCHIVENAME=""
 ARCHIVENAME+=$NAME
@@ -52,7 +64,8 @@ ARCHIVENAME+="_cage.tar.xz"
 
 cd ~/grc/samples
 
-tar -cf ~/$ARCHIVENAME $NAME/*
+#tar -cf ~/$ARCHIVENAME $NAME/*
+tar -cf /media/usb0/$ARCHIVENAME $NAME/*
 rm -r ~/grc/samples/$NAME
 
 cd ~/grc/scripts
